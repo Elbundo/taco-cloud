7e233990b327b7db6323cd06c46f5577020f88a8
Elbundo <pivoetozlo@gmail.com>
2022-09-22 17:31:25 +0300

    initial commit

---
+++ src/main/java/com/elbundo/tacocloud/model/Taco.java
@@ -0,0 +1,11 @@
+package com.elbundo.tacocloud.model;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+    private String name;
+    private List<Ingredient> ingredients;
+}
